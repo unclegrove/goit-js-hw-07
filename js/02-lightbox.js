00d76efd7883c77813f9d673fdfa692edbd45f87
Anton Alutin <mrunclegrove@gmail.com>
2022-08-19 16:09:12 +0300

    finish hw-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulRef = document.querySelector('.gallery');
+
+const createGalleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+  })
+  .join('');
+
+ulRef.insertAdjacentHTML('afterbegin', createGalleryMarkup);
+
+let lightboxGallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionsDelay: '250ms',
+});
